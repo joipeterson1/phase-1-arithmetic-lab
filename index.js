9952fa6a81923f7f7816d8adf46abab0442a1b2b
Dalecia <petersondalecia@gmail.com>
2023-10-31 14:00:01 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let multiply = 62
+let num1 = 31;
+let num2 = 2;
+31 * 2 === 62
 
-// Write your code here
+let random = Math.ceil(Math.random())
+
+let num3 = 16
+let num4 = 6
+let mod = num3 % num4 
+
+
+let min = 0
+let max = 20
+Math.round (Math.random() * (max - min + 1)) + min
